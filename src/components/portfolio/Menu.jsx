625306321981ef85c86b75ba5e10098dfc00c53d
DingHeng <jayleedingheng0401@gmail.com>
2025-06-04 22:51:19 +0800

    feat; more Data update for Resume component

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -1,20 +1,28 @@
 import Work1 from "../../assets/work-1.svg";
 import Work2 from "../../assets/work-2.svg";
+import Work3 from "../../assets/work-3.svg";
 
 const Menu = [
   {
     id: 1,
     image: Work2,
-    title: "Personal Portfolio Website (it's this website!)",
+    title: "Personal Portfolio Website - ReactJS",
     category: "Front End",
     url: "https://github.com/JayLDH41/portfolio-website-react",
   },
   {
     id: 2,
     image: Work1,
-    title: "Academy Online Course Website (link in GitHub)",
+    title: "Academy Online Course Website - ReactJS",
     category: "Front End",
     url: "https://github.com/junsheng526/lucas-cyber-academy",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Tic Tac Toe - Vanilla Javascript Practice",
+    category: "Front End",
+    url: "https://github.com/JayLDH41/frontend-practice-tictactoe",
   },
 ];
 
